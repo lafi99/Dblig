de88c72ed5d1dcff0976f7269143970089ba109d
bushr <ahmad.m.lafi@gmail.com>
2022-06-29 19:47:52 +0300

    .

--- DownloadPlaylist.py
+++ DownloadPlaylist.py
@@ -1,20 +1,46 @@
+"""
+install dependencies first:
+pip install pafy
+pip install youtube-dl==2020.12.2
+"""
 import time
 import pafy
 
 # youtube API key
 pafy.set_api_key("AIzaSyCtU2gIKBhRU5Ofa_9EwTFNr3glBxCNJkM")
 
+
+def writeVideoInfo(video, path=""):
+    # open file to write vide info
+    videoInfoFile = open(file=path+str(video.title).replace("|", "") + ".txt", mode="w")
+    # write video info
+    videoInfoFile.write(f"title: {video.title}\n")
+    videoInfoFile.write(f"author: {video.author}\n")
+    videoInfoFile.write(f"category: {video.category}\n")
+    videoInfoFile.write(f"published: {video.published}\n")
+    videoInfoFile.write(f"description: {video.description}\n")
+    videoInfoFile.write(f"keywords: {video.keywords}\n")
+    videoInfoFile.write(f"videoid: {video.videoid}\n")
+    videoInfoFile.write(f"bigthumb: {video.bigthumb}\n")
+    videoInfoFile.write(f"viewcount: {video.viewcount}\n")
+    videoInfoFile.write(f"likes: {video.likes}\n")
+    videoInfoFile.write(f"dislikes: {video.dislikes}\n")
+    videoInfoFile.write(f"rating: {video.rating}\n")
+    videoInfoFile.write(f"duration: {video.duration}\n")
+    videoInfoFile.write(f"length: {video.length}\n")
+    # close the file
+    videoInfoFile.close()
+
+
 # ###### playlist ##########
-# getting playlist
 # url of playlist
-url = "https://www.youtube.com/playlist?list=PLqM7alHXFySGqCvcwfqqMrteqWukz9ZoE"
+url = "<PLAYLIST_URL>"
 
+# getting playlist
 playlist = pafy.get_playlist2(playlist_url=url)
-
-# printing playlist
-print(playlist)
 
 for item in range(len(playlist)):
     url = playlist[item].getbestaudio()
-    url.download(filepath=r"C:\Users\bushr\PycharmProjects\Blagin\videos")
+    writeVideoInfo(playlist[0])
+    url.download(filepath=r"<PATH_TO_SAVE_PLAYLIST_IN>")
     time.sleep(3)
